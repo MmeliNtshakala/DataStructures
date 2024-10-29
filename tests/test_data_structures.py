4914d963e3613db0d1f539b28d0c4a0e9c9e90c2
Mmeli Ntshakala <135122360+MmeliNtshakala@users.noreply.github.com>
2024-10-29 08:52:30 +0200

    Update test_data

--- tests/test_data_structures.py
+++ tests/test_data_structures.py
@@ -21,6 +21,7 @@ class MyTestCase(unittest.TestCase):
     def test_find_all_even_numbers(self):
         self.assertEqual(find_even_numbers([1, 2, 3, 4, 5, 6]), (2, 4, 6))
         self.assertEqual(find_even_numbers([10, 9, 8, 7, 6, 5]), (10, 8, 6))
+        self.assertEqual(find_even_numbers([1, 53, 51]), [])
 
     def test_find_all_odd_numbers(self):
         self.assertEqual(find_odd_numbers([1, 2, 3, 4, 5]), (1, 3, 5))
